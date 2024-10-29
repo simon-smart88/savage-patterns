3910f5d7ea26225f36bab84aea992f003d42f06b
simon-smart88 <simon.smart@cantab.net>
2024-10-29 14:22:57 +0000

    make a start on line_module

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(shinyjqui)
 library(shinyjs)
 source("ring_module.R")
 source("square_module.R")
+source("line_module.R")
 
 jsCode <- "shinyjs.svgprint = function(){
 document.getElementsByTagName('svg');
@@ -17,9 +18,10 @@ ui <- fluidPage(
   #title = "Savage patterns",
   titlePanel("Savage patterns"),
         sidebarPanel(width = c(4,8),
-            selectInput("module", "Pattern", choices = c("Square" = "square", "Ring" = "ring"), selected = "square"),
+            selectInput("module", "Pattern", choices = c("Square" = "square", "Ring" = "ring", "Line" = "line"), selected = "line"),
             conditionalPanel("input.module == 'ring'", ring_module_ui("ring_module")),
             conditionalPanel("input.module == 'square'", square_module_ui("square_module")),
+            conditionalPanel("input.module == 'line'", line_module_ui("line_module")),
             downloadButton("download"),
             actionButton("get_html", "Get html")
         ),
@@ -34,7 +36,8 @@ server <- function(input, output, session){
 
   ring_module_server("ring_module", patterns)
   square_module_server("square_module", patterns)
-
+  line_module_server("line_module", patterns)
+  
   #send to UI
   output$svgout <- renderUI({
     patterns[[input$module]]
---
+++ line_module.R
@@ -0,0 +1,67 @@
+line_module_ui <- function(id){
+  ns <- shiny::NS(id)
+  tagList(
+    sliderInput(ns("outer"), "Outer", value = 11, step = 1, min = 6, max = 20),
+    sliderInput(ns("inner"), "Inner", value = 5, step = 1, min = 2, max = 11),
+
+  )
+}
+
+line_module_server <- function(id, patterns){
+  moduleServer(id, function(input, output, session) {
+  
+
+    svg_line <- function(x){
+      tags$line(x1 = x[1], x2 = x[2], y1 = x[3], y2 = x[4])
+    }
+      
+  
+  svg_pattern <- reactive({
+    
+    outer_points <- input$outer
+    inner_points <- input$inner
+    
+    inner_offset <- (outer_points - inner_points) / 2
+    
+    outer_x <- c(seq(0, outer_points - 1), rep(c(0, outer_points - 1), outer_points - 2), seq(0, outer_points - 1))
+    outer_y <- c(rep(0, outer_points), rep(seq(1, outer_points - 2), each = 2), rep(outer_points - 1, outer_points))
+    
+    inner_x <- c(seq(0, inner_points - 1), rep(c(0, inner_points - 1), inner_points - 2), seq(0, inner_points - 1)) + inner_offset
+    inner_y <- c(rep(0, inner_points), rep(seq(1, inner_points - 2), each = 2), rep(inner_points - 1, inner_points)) + inner_offset
+    
+    x1 <- rep(outer_x, length(inner_x))
+    x2 <- rep(inner_x, length(outer_x))
+    
+    y1 <- rep(outer_y, length(inner_y))
+    y2 <- rep(inner_y, length(outer_y))
+    
+    element_mat <- matrix(c(x1, x2, y1, y2), ncol = 4)
+      
+    elements <- apply(element_mat, 1, svg_line)
+    
+    top_corner <- 0
+    bottom_corner <- input$outer
+    
+    
+    #create the final svg
+    tagList(tags$svg(xmlns = "http://www.w3.org/2000/svg",
+                     `xmlns:xlink`="http://www.w3.org/1999/xlink",
+                     version="1.1",
+                     viewBox = glue("{top_corner} {top_corner} {bottom_corner} {bottom_corner}"),
+                     height = "100%",
+                     id = "pattern",
+                     tags$style(paste0("
+
+                                line { stroke-width: 0.01px;
+                                      stroke: black}
+
+                       ")),
+                     
+                     elements))
+    
+  })
+  
+  observe(patterns$line <- svg_pattern())
+  
+  })
+}

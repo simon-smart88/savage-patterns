19c0e4df5ad8743aeb9604140f6ad5eb259963e6
simon-smart88 <simon.smart@cantab.net>
2024-12-18 12:22:27 +0000

    add colour / opacity

--- line_module.R
+++ line_module.R
@@ -30,7 +30,7 @@ line_module_ui <- function(id){
       accordion_panel("Animation",
         sliderInput(ns("breath"),
                     span("Change in size",
-                        tooltip(icon("info-circle"), "Controls how much the Inner point offset increases")),
+                        tooltip(icon("info-circle"), "Controls how much the inner point offset increases")),
                     value = 20, step = 1, min = 10, max = 50),
         sliderInput(ns("speed"),
                     span("Animation duration",
@@ -38,7 +38,23 @@ line_module_ui <- function(id){
                     value = 30, step = 1, min = 5, max = 60)
       ),
       accordion_panel("Colour",
-        sliderInput(ns("stroke"), "Line thickness", value = 1, step = 0.05, min = 0.1, max = 2)
+        sliderInput(ns("stroke"), "Line thickness", value = 1, step = 0.05, min = 0.1, max = 2),
+        sliderInput(ns("opacity"),
+                    span("Opacity",
+                         tooltip(icon("info-circle"), "Move the lines to the inner points away from each other towards the nearest outer edge")),
+                    value = c(25,75), step = 1, min = 0, max = 100),
+        uiOutput(ns("colour_picker")),
+        selectInput(ns("hue"),
+                    span("Hue",
+                         tooltip(icon("info-circle"), "Choose a colour palette to use")),
+                    c("Random" = "random", "Red" = "red", "Orange" = "orange",
+                      "Yellow" = "yellow", "Green" = "green", "Blue" =  "blue",
+                      "Purple" = "purple", "Pink" = "pink", "Monochrome" = "monochrome")),
+        selectInput(ns("lumin"),
+                    span("Luminosity",
+                         tooltip(icon("info-circle"), "Choose how bright the colour palette is")),
+                    c("Random" = "random", "Light" = "light",
+                      "Bright" = "bright", "Dark" = "dark"))
       )
     )
   )
@@ -46,6 +62,26 @@ line_module_ui <- function(id){
 
 line_module_server <- function(id, patterns){
   moduleServer(id, function(input, output, session) {
+
+    invalidate_trigger <- reactiveVal(0)
+
+    init_cols <- reactive({
+      invalidate_trigger()
+      randomcoloR::randomColor(count = 3, hue = input$hue, luminosity = input$lumin)
+    })
+
+    output$colour_picker <- renderUI({
+      n_cols <- 3
+      ids <- session$ns(paste0("colour_", 1:n_cols))
+      tagList(
+        tags$label(
+          span("Pick colours",
+               tooltip(icon("info-circle"), "Manually select the colours that the pattern cycles through"))
+        ),
+        lapply(seq_along(ids), function(i) {colourpicker::colourInput(
+          ids[i], "", value = init_cols()[i],
+          showColour = "background", closeOnClick = TRUE)}))
+    })
 
     observeEvent(input$random, {
       updateSliderInput(session, "outer", value = 6 + sample.int(14, size = 1))
@@ -61,10 +97,39 @@ line_module_server <- function(id, patterns){
       shinyjs::runjs(glue("
       document.getElementById('pattern').style.setProperty('--stroke', '{input$stroke / 100}');"))
     })
+#
+#     input_colours <- reactive({
+#       n_cols <- 3
+#       ids <- paste0("colour_", 1:n_cols)
+#       c(lapply(seq_along(ids), function(i) {input[[ids[i]]]}))
+#     })
+
+    observe({
+      # req(length(input_colours()) > 0)
+      # colours <- input_colours()
+      shinyjs::runjs(glue("
+      document.getElementById('pattern').style.setProperty('--colour_1', '{input$colour_1}');
+      document.getElementById('pattern').style.setProperty('--colour_2', '{input$colour_2}');
+      document.getElementById('pattern').style.setProperty('--colour_3', '{input$colour_3}');"))
+    })
+
+    observe({
+      shinyjs::runjs(glue("
+      document.getElementById('pattern').style.setProperty('--opacity_start', '{input$opacity[1] / 100}');
+      document.getElementById('pattern').style.setProperty('--opacity_mid', '{mean(input$opacity) / 100}');
+      document.getElementById('pattern').style.setProperty('--opacity_end', '{input$opacity[2] / 100}');"
+                          ))
+    })
+
+    observe({
+      shinyjs::runjs(glue("
+      document.getElementById('pattern').style.setProperty('--stroke', '{input$stroke / 100}');"))
+    })
 
     svg_line <- function(x, speed){
       tags$line(
         x1 = x[1], x2 = x[2], y1 = x[3], y2 = x[4],
+        stroke = glue::glue("url(#gradient-opacity{x[7]})"),
         tags$animate(
           attributeName = "x2",
           values = glue("{x[2]}; {x[5]}; {x[2]}"),
@@ -116,6 +181,8 @@ line_module_server <- function(id, patterns){
     side_bottom <- outer_y_set == outer_points - 1 & outer_x_set < outer_points - 1 & outer_x_set > 0
     side_left <- outer_x_set == 0
     side_right <- outer_x_set == outer_points - 1
+    side_index <- (as.numeric(side_left) * 1) + (as.numeric(side_top) * 2) +
+                  (as.numeric(side_right) * 3) + (as.numeric(side_bottom) * 4)
 
     point_offset <- (inner_x[2] - inner_x[1]) * (input$inner_offset / 100)
     inner_y1_set[side_top] <- inner_y1_set[side_top] - point_offset
@@ -124,7 +191,7 @@ line_module_server <- function(id, patterns){
     inner_x1_set[side_right] <- inner_x1_set[side_right] + point_offset
 
     # apply to the function
-    element_mat <- matrix(c(outer_x_set, inner_x1_set, outer_y_set, inner_y1_set, inner_x2_set, inner_y2_set), ncol = 6)
+    element_mat <- matrix(c(outer_x_set, inner_x1_set, outer_y_set, inner_y1_set, inner_x2_set, inner_y2_set, side_index), ncol = 7)
     elements <- apply(element_mat, 1, svg_line,
                       speed = input$speed)
 
@@ -146,17 +213,50 @@ line_module_server <- function(id, patterns){
         height = "100%",
         id = "pattern",
         tags$style(
-          paste0("
+          paste0('
            :root{
-             --stroke: 0.01px
+             --stroke: 0.01px;
+             --opacity_start: 0;
+             --opacity_mid: 0.5;
+             --opacity_end: 1;
+             --colour_1: "black";
+             --colour_2: "black";
+             --colour_3: "black";
            }
            line {
             stroke-width: var(--stroke);
-            stroke: black}
-           ")),
+            }
+           ')),
+           tags$defs(
+             tags$linearGradient(
+               id = "gradient-opacity1", x1 = "0%", y1 = "0%", x2 = "100%", y2 = "0%",
+               tags$stop(offset = "0%", `stop-color` =  "var(--colour_1)", `stop-opacity` = "var(--opacity_start)"),
+               tags$stop(offset = "50%", `stop-color` =  "var(--colour_2)", `stop-opacity` = "var(--opacity_mid)"),
+               tags$stop(offset = "100%", `stop-color` = "var(--colour_3)", `stop-opacity` = "var(--opacity_end)")
+             ),
+             tags$linearGradient(
+               id = "gradient-opacity2", x1 = "0%", y1 = "0%", x2 = "0%", y2 = "100%",
+               tags$stop(offset = "0%", `stop-color` =  "var(--colour_1)", `stop-opacity` = "var(--opacity_start)"),
+               tags$stop(offset = "50%", `stop-color` =  "var(--colour_2)", `stop-opacity` = "var(--opacity_mid)"),
+               tags$stop(offset = "100%", `stop-color` = "var(--colour_3)", `stop-opacity` = "var(--opacity_end)")
+             ),
+             tags$linearGradient(
+               id = "gradient-opacity3", x1 = "100%", y1 = "0%", x2 = "0%", y2 = "0%",
+               tags$stop(offset = "0%", `stop-color` =  "var(--colour_1)", `stop-opacity` = "var(--opacity_start)"),
+               tags$stop(offset = "50%", `stop-color` =  "var(--colour_2)", `stop-opacity` = "var(--opacity_mid)"),
+               tags$stop(offset = "100%", `stop-color` = "var(--colour_3)", `stop-opacity` = "var(--opacity_end)")
+             ),
+             tags$linearGradient(
+               id = "gradient-opacity4", x1 = "0%", y1 = "100%", x2 = "0%", y2 = "0%",
+               tags$stop(offset = "0%", `stop-color` =  "var(--colour_1)", `stop-opacity` = "var(--opacity_start)"),
+               tags$stop(offset = "50%", `stop-color` =  "var(--colour_2)", `stop-opacity` = "var(--opacity_mid)"),
+               tags$stop(offset = "100%", `stop-color` = "var(--colour_3)", `stop-opacity` = "var(--opacity_end)")
+           )),
          elements))
 
   })
+
+
 
   observe(patterns$line <- svg_pattern())
 
